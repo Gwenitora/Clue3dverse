73896afd8c292972a083e3ed0c4ef7a012545d2b
Gwenitora <gwenitora.gardu74@gmail.com>
2023-10-11 09:45:53 +0200

    reinit

--- public/src/app.ts
+++ public/src/app.ts
@@ -1,15 +1,43 @@
-import { AppConfig } from "./AppConfig";
-import * as api3dverse from '@3dverse/api';
+import { AppConfig } from "./AppConfig.js";
 
-window.addEventListener('load', async () =>
+class App {
+    constructor() {
+        console.log(`AppConfig.SCENE_UUID: ${AppConfig.SCENE_UUID}`);
+    }
+
+    public startingScene() {
+        SDK3DVerse.webAPI.createOrJoinSession(AppConfig.SCENE_UUID).then((connectionInfo: any) => {
+    
+            SDK3DVerse.notifier.on('onLoadingStarted', () => {
+                let message = document.getElementById("message");
+                if (message) {
+                    message.innerHTML = "Connecting..."
+                }
+            });
+            SDK3DVerse.notifier.on('onLoadingProgress', (status: {message: string}) => {
+                let message = document.getElementById("message");
+                if (message) {
+                    message.innerHTML = status.message
+                }
+            });
+            SDK3DVerse.notifier.on('onLoadingEnded', (status: {message: string}) => {
+                let message = document.getElementById("message");
+                if (message) {
+                    message.innerHTML = status.message
+                }
+            });
+        
+            SDK3DVerse.setupDisplay(document.getElementById('display_canvas'));
+            SDK3DVerse.startStreamer(connectionInfo);
+    
+            console.log("App started");
+        });
+    }
+}
+
+const app = new App();
+
+window.addEventListener('load', () =>
 {
-    //api3dverse.createSession({scene_id: AppConfig.SCENE_UUID})
-    const connectionInfo = await SDK3DVerse.webAPI.createOrJoinSession(AppConfig.sceneUUID);
-
-    SDK3DVerse.notifier.on('onLoadingStarted', () => document.getElementById("message").innerHTML = "Connecting...");
-    SDK3DVerse.notifier.on('onLoadingProgress', (status) => document.getElementById("message").innerHTML = status.message);
-    SDK3DVerse.notifier.on('onLoadingEnded', (status) => document.getElementById("message").innerHTML = status.message);
-
-    SDK3DVerse.setupDisplay(document.getElementById('display_canvas'));
-    SDK3DVerse.startStreamer(connectionInfo);
+    app.startingScene();
 });
