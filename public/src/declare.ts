3f37bbdca28c5e5fd7c0284af633dec3c9b3259f
Gwenitora <gwenitora.gardu74@gmail.com>
2023-10-11 10:22:04 +0200

    update: declare

--- public/src/declare.ts
+++ public/src/declare.ts
@@ -1,8 +1,12 @@
 declare module 'https://*'
 
 declare module 'https://cdn.3dverse.com/legacy/sdk/stable/SDK3DVerse.js' {
-    export const webAPI: any;
-    export const notifier: any;
+    export namespace webAPI {
+      export function createOrJoinSession(sceneId: string): Promise<any>;
+    }
+    export module notifier {
+      function on(event: string, callback: Function): void;
+    }
     export const setupDisplay: (canvas: HTMLElement | null) => void;
     export const startStreamer: (connectionInfo: any) => void;
 }
